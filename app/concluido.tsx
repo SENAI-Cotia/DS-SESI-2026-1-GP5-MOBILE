554b7d5ca61338f516c47e01ef6b99e7cc4d3e02
JUAN MATIAS PIMENTEL DA SILVA <juan.pimentel@portalsesisp.org.br>
2026-08-03 08:43:16 -0300

    feat: adicionando

--- app/concluido.tsx
+++ app/concluido.tsx
@@ -12,7 +12,7 @@ export default function Concluido() {
 
         <View style={[style.fundo, darkMode && style.fundoDark]}>
             <View style={style.bolaTopo} />
-            <Text>Teste novo teste</Text>
+            <Text>Teste novo teste teste</Text>
             <View style={style.bolaBaixo} />
 
             <Image source={require("../assets/images/logo.png")} style={style.logo}></Image>
